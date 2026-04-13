150d46b52cb85181780ae763a0c5ae0f67c0e815
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-13 19:43:55 +0200

    faire un réorganisation du code et de header

--- valisp/allocateur.h
+++ valisp/allocateur.h
@@ -1,10 +1,13 @@
 #ifndef ALLOCATEUR_H
 #define ALLOCATEUR_H
+#include <stdint.h>
+
+#define TAILLE_MEMOIRE_DYNAMIQUE 32768
 /*
     Maitre les headers des types ici
 
 */
-
+typedef uint32_t bloc;
 
 /*
     Maitre les headers des fonctions ici
@@ -12,4 +15,9 @@
 
 void initialiser_memoire_dynamique();
 bloc cons_bloc(int rm, int precedant, int libre, int suivant);
+int bloc_suivant(int indice);
+int bloc_precedant(int indice);
+int usage_bloc(int indice);
+int rm_bloc(int indice);
+
 #endif
