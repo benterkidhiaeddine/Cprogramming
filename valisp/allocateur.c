cff76883cb83de186493de7d78a968a9215bb4eb
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-13 19:10:10 +0200

    corrigier le nom de block

--- valisp/allocateur.c
+++ valisp/allocateur.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #define  TAILLE_MEMOIRE_DYNAMIQUE 32768
-typedef uint32_t block;
+typedef uint32_t bloc;
 
 
-block MEMOIRE_DYNAMIQUE[TAILLE_MEMOIRE_DYNAMIQUE];
+bloc MEMOIRE_DYNAMIQUE[TAILLE_MEMOIRE_DYNAMIQUE];
 
 
 void initialiser_memoire_dynamique(){
     MEMOIRE_DYNAMIQUE[0] =  (1 << 15) - 1;
     MEMOIRE_DYNAMIQUE[TAILLE_MEMOIRE_DYNAMIQUE -  1] = (1 << 16) - 1; 
 }
+
--- valisp/allocateur.h
+++ valisp/allocateur.h
@@ -1,7 +1,15 @@
 #ifndef ALLOCATEUR_H
 #define ALLOCATEUR_H
 /*
+    Maitre les headers des types ici
+
+*/
+
+
+/*
     Maitre les headers des fonctions ici
 */
+
 void initialiser_memoire_dynamique();
+bloc cons_bloc(int rm, int precedant, int libre, int suivant);
 #endif
