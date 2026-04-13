32cd62b2ab83a4da3246435d104fb9719dface02
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-13 18:53:34 +0200

    ajouter la structure de données qui va contenir toute la mémoire de l'allocateur

--- valisp/allocateur.c
+++ valisp/allocateur.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
+#include <stdint.h>
 #define  TAILLE_MEMOIRE_DYNAMIQUE 32768
-typedef struct uint32_t block;
+typedef uint32_t block;
 
 
-
-int main(void){
-    int taille = TAILLE_MEMOIRE_DYNAMIQUE;
-    printf("%d\n", taille ); 
-
-    return 0;
-}
+block MEMOIRE_DYNAMIQUE[TAILLE_MEMOIRE_DYNAMIQUE];
