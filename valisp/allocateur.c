9a79cc3e8f3d6e25be3a48d8dc72d0e5d8ce3563
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-13 19:17:19 +0200

    add cons_bloc function

--- valisp/allocateur.c
+++ valisp/allocateur.c
@@ -12,3 +12,35 @@ void initialiser_memoire_dynamique(){
     MEMOIRE_DYNAMIQUE[TAILLE_MEMOIRE_DYNAMIQUE -  1] = (1 << 16) - 1; 
 }
 
+bloc cons_bloc(int rm, int precedant, int libre, int suivant){
+    /*
+        Ajouter la marqiue pour le rammasse miete 
+    */
+    bloc res = rm;
+    
+    /*
+        Ajouter la valeur du précéent 
+    */
+    res = res << 15;
+    res = res | precedant;
+
+ 
+    /*
+        Ajouter la valeur du libre 
+    */
+    res = res << 1;
+    res = res | libre;
+
+
+ 
+    /*
+        Ajouter la valeur du suivant 
+    */
+    res = res << 15;
+    res = res | suivant;
+
+
+    return res;
+
+
+}
