ed9c99d23c8df251c10406984cc7f05420bac764
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-13 18:43:07 +0200

    reallocate allocateur files

---
+++ valisp/allocateur.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define  TAILLE_MEMOIRE_DYNAMIQUE 32768
+typedef struct uint32_t block;
+
+
+
+int main(void){
+    int taille = TAILLE_MEMOIRE_DYNAMIQUE;
+    printf("%d\n", taille ); 
+
+    return 0;
+}
---
+++ valisp/allocateur.h
@@ -0,0 +1,7 @@
+#ifndef ALLOCATEUR_H
+#define ALLOCATEUR_H
+/*
+    Maitre les headers des fonctions ici
+*/
+
+#endif
