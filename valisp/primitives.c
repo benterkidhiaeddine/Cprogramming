7cd2c8ad4d2ddddfc460b008218b7f2f80a3d87d
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-04-21 21:50:52 +0200

    implemneeter primitive add_valisp

---
+++ valisp/primitives.c
@@ -0,0 +1,33 @@
+#include "primitives.h"
+#include <stdio.h>
+#include "erreurs.h"
+
+int longeur_liste(sexpr liste){
+    if (liste == NULL){
+        return 0;
+    }
+    return (1+ longeur_liste(cdr(liste)));
+}
+
+
+void test_nb_parametres(sexpr liste, char* fonction, int taille) {
+
+    if (longeur_liste(liste) != taille){
+        erreur(ARITE, fonction, "nombre de paramètres", liste);
+    }
+}
+
+sexpr add_valisp(sexpr liste, sexpr env){
+    sexpr a;
+    sexpr b;
+    test_nb_parametres(liste, "+" ,2);
+    
+    a = car(liste);
+    b = car(cdr(liste));
+
+    if (!integer_p(a)) erreur(TYPAGE, "+" ,"nécessite un entier", a);
+    if (!integer_p(b)) erreur(TYPAGE, "+" ,"nécessite un entier", b);
+
+    return new_integer(get_integer(a) + get_integer(b));
+
+}
---
+++ valisp/primitives.h
@@ -0,0 +1,10 @@
+#ifndef PRIMITIVES_H
+#define PRIMITIVES_H
+#include "types.h"
+    
+
+void test_nb_parametres(sexpr liste, char* fonction, int taille);
+sexpr add_valisp(sexpr liste, sexpr env);
+
+
+#endif
